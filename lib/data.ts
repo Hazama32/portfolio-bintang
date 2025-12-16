d509db0dee776637056e6e67a2d5cb34cbc4c83e
Hazama32 <bintanghack32@gmail.com>
2025-12-16 20:26:58 +0700

    update data

--- lib/data.ts
+++ lib/data.ts
@@ -45,15 +45,15 @@ export const experiencesData = [
   },
   {
     title: "Front-End Developer",
-    location: "Orlando, FL",
+    location: "Areta Informatics College",
     description:
       "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
     icon: React.createElement(CgWorkAlt),
     date: "November 2019 - 2021",
   },
   {
-    title: "Full-Stack Developer",
-    location: "Houston, TX",
+    title: "Barista & Freelance Full-Stack Developer",
+    location: "Freelance",
     description:
       "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
     icon: React.createElement(CgWorkAlt),
